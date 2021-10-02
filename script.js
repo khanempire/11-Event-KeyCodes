d47ab667a31698f767f29b6c1974f5f7720af907
khanempire <mdaamirkhan7186@gmail.com>
2021-10-02 09:02:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById("insert");
+window.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  console.log(e.code);
+  console.log(e.keyCode);
+  insert.innerHTML = ` 
+  <div class="key">
+  ${e.key === " " ? "Space" : e.key}
+  <small>event.key</small>
+</div>
+<div class="key">
+  ${e.keyCode === " " ? "Space" : e.keyCode}
+  <small>event.keyCode</small>
+</div>
+<div class="key">
+  ${e.code === " " ? "Space" : e.code}
+  <small>event.code</small>
+</div>
+`;
+});
